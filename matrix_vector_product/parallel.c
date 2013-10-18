379b0d8fb8cacbf2c831479b09a7d0f116302ca6
Maciej Malecki <smt116@gmail.com>
2013-10-18 15:01:24 +0200

    Removed unused code from matrix vector product.

--- matrix_vector_product/parallel.c
+++ matrix_vector_product/parallel.c
@@ -4,27 +4,3 @@
  */
 #include "lib.h"
 #include "parallel.h"
-
-void parallel_matrix_product(Matrix *product, Matrix *A, Matrix *x) {
-  int i,
-      k,
-      l;
-
-/*
-    if(rank == 0) {
-        MPI_Send(i, 1, MPI_INT, (i % size), 0, MPI_COMM_WORLD);
-      }
-    } else {
-      MPI_Recv(i, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    }
-    */
-  for(i=0; i < A->n; i++) {
-    for(k=0; k < A->m; k++) {
-      for(l=0; l < x->m; l++) {
-        product->data[(l * product->n) + k] +=
-          A->data[(i * A->n) + k] * x->data[(l * x->n) + k];
-      }
-    }
-  }
-
-}
--- matrix_vector_product/parallel.h
+++ matrix_vector_product/parallel.h
@@ -5,6 +5,4 @@
 #ifndef SMT__LOCAL_PARALLEL_LIB
 #define SMT__LOCAL_PARALLEL_LIB
 
-  void parallel_matrix_product(Matrix *, Matrix *, Matrix *);
-
 #endif
